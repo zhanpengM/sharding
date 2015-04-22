c0b97177a6fe54fc98c5d7ad4f121869a86da97d
zhanpengM <6297958@qq.com>
2015-04-22 17:24:44 +0800

    new  create

---
+++ sharding/src/main/java/zhanpeng/zookeeper/sharding/App.java
@@ -0,0 +1,13 @@
+package zhanpeng.zookeeper.sharding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
